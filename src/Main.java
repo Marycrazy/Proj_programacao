d16d9cbe9d8b1e3322253ba0f1762db3d1e646c7
Marycrazy <marianapaisjorgemagalhaes@email.com>
2024-11-27 22:43:00 +0000

    Adicionar exibição de dados de clientes e técnicos; corrigir fluxo de exibição no Main e atualizar credenciais de acesso.

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         try {
             Sistema.getInstance().carregarDados();
             pressEnterKey();
-            //Sistema.getInstance().exibirDados();
-            //pressEnterKey();
+            Sistema.getInstance().exibirDados();
+            pressEnterKey();
             mainLoop();
         } catch (Exception e) {
             System.out.println("Exception: " + e);
